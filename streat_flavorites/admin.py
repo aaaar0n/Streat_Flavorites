bf64a66ffca6c3f12894426d194b7660b1e01425
Aaron <ardelacruz1624@gmail.com>
2023-08-08 16:49:41 +0800

    Added images for the items

--- streat_flavorites/admin.py
+++ streat_flavorites/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Category, Subcategory, Item
 
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'stocks', 'subcategory', 'photo')
+
 admin.site.register(Category)
 admin.site.register(Subcategory)
-admin.site.register(Item)
